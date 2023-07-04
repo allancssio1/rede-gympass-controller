7606dce5ae87f35981bf9b9c9553da4c420042ad
allancassio <allan.cassio1@gmail.com>
2023-07-04 15:22:08 -0300

    ...

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma-use-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-use-repository'
+import { UserAlreadyExistsError } from '@/useCases/errors/userAlreadyExistsError'
 import { RegisterUseCase } from '@/useCases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
@@ -18,7 +19,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError)
+      return reply.status(409).send({ message: error.message })
+
+    return reply.status(500).send() // TODO: to fix
   }
 
   return reply.status(201).send()
--- src/useCases/register.ts
+++ src/useCases/register.ts
@@ -1,6 +1,6 @@
-import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/userAlreadyExistsError'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -12,13 +12,13 @@ export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
   async execute({ email, name, password }: RegisterUseCaseRequest) {
-    const userAlreadyExists = await prisma.user.findUnique({ where: { email } })
+    const userAlreadyExists = this.usersRepository.findUserByEmail(email)
 
-    if (userAlreadyExists) throw new Error('User already exists.')
+    if (userAlreadyExists !== null) {
+      throw new UserAlreadyExistsError()
+    }
 
     const password_hash = await hash(password, 6)
-
-    // const prismaUsersRepository = new PrismaUsersRepository()
 
     await this.usersRepository.create({ name, email, password_hash })
   }
