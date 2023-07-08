8465654a83fa7990c5a9fd0d8ad1674bccfde90b
allancassio <allan.cassio1@gmail.com>
2023-07-08 19:27:02 -0300

    create authenticate controller

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,31 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-use-repository";
+import { AuthenticateUseCase } from "@/useCases/authenticate";
+import { InvalidCredentialsError } from "@/useCases/errors/invalidCredentialsError";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function authenticate (request: FastifyRequest, reply: FastifyReply) {
+  const authenticateSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
+  })
+
+  const {email,password} = authenticateSchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+    await authenticateUseCase.execute({email, password})
+
+  } catch (error) {
+    if(error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({message: error.message})
+    }
+
+    throw error
+  }
+
+  return reply.status(200).send()
+
+}
