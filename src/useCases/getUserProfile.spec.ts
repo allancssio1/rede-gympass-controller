4204468da49d6c141b4cfae2fa3755187ff550fa
allancassio <allan.cassio1@gmail.com>
2023-07-10 17:14:17 -0300

    create tests to get user profile use case

---
+++ src/useCases/getUserProfile.spec.ts
@@ -0,0 +1,35 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/inMemory/inMemoryUsersRepository";
+import { hash } from "bcryptjs";
+import { GetUserProfileUseCase } from "./getUserProfile";
+import { ResourceNotFoundError } from "./errors/resourceNotFoundError";
+
+let usersRepository : InMemoryUsersRepository
+let getUserProfileUseCase : GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    getUserProfileUseCase = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('Should be able to get user profile', async () => {
+    const {id} = await usersRepository.create({
+      name: "Allan Cássio",
+      email: "allan.cassio1@gmail.com",
+      password_hash: await hash('123456', 6)
+    })
+
+    const {user} = await getUserProfileUseCase.execute({
+      userId: id
+    })
+
+    expect(user.name).toEqual("Allan Cássio")
+  })
+
+  it('Should be able to get user profile with wrong id', async () => {
+    await expect( () => getUserProfileUseCase.execute({
+      userId: 'not-existing-id'
+    })).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})  
