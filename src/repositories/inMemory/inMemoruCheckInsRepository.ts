16f33790a5255672ce2ccabe1763b5c3810dfe88
allancassio <allan.cassio1@gmail.com>
2023-07-18 10:53:29 -0300

    tests on date create check in

--- src/repositories/inMemory/inMemoruCheckInsRepository.ts
+++ src/repositories/inMemory/inMemoruCheckInsRepository.ts
@@ -2,6 +2,7 @@ import { CheckIn, Prisma } from "@prisma/client";
 import { randomUUID } from "node:crypto";
 import { CheckInsRepository } from "../check-ins-repository";
 import { GetResult } from "@prisma/client/runtime";
+import dayjs from "dayjs";
 
 export class InMemoryCheckiInsRepository implements CheckInsRepository {
   public itens: CheckIn[] = []
@@ -21,7 +22,16 @@ export class InMemoryCheckiInsRepository implements CheckInsRepository {
   }
   
   async findByUserIdOnDate(userId: string, date: Date) {
-    const checkInByUserId = this.itens.find(userFound => userFound.user_id === userId)
+    // instalada lib dayjs para trabalhar com datas
+    const startOfTheDay = dayjs(date).startOf('date') // startof retorna data YYYY-MM-DDT00:00:00
+    const endOfTheDay = dayjs(date).endOf('date') // endOf retorna data YYYY-MM-DDT23:59:59
+
+    const checkInByUserId = this.itens.find(checkIn => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)  
+      
+      return checkIn.user_id === userId && isOnSameDate
+    })
 
     if(!checkInByUserId) {
       return null
--- src/useCases/checkIn.spec.ts
+++ src/useCases/checkIn.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryCheckiInsRepository } from "@/repositories/inMemory/inMemoruCheckInsRepository";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { CheckInUseCase } from "./checkIn";
+import { string } from "zod";
 
 let checkInRepository: InMemoryCheckiInsRepository
 let checkInUseCase: CheckInUseCase
@@ -40,5 +41,25 @@ describe('Register Check In', () => {
         userId: "user-01"
     })
     ).rejects.toBeInstanceOf(Error)
+  })
+
+  it("Should  be able to check in twice in different days",async () => {
+    //seta a data falsa para o dia 26/01/1991 às 10h:00m:00s + 3h
+    vi.setSystemTime(new Date(2022, 0, 10, 8, 0, 0))
+    await checkInUseCase.execute({
+      gymId: "gym-01",
+      userId: "user-01"
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 11, 8, 0, 0))
+
+    const {checkIn} = await checkInUseCase.execute({
+      gymId: "gym-01",
+      userId: "user-01"
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+
+
   })
 })
