d6ff719dd886e75217d24e8f4c5fb24050fd7875
allancassio <allan.cassio1@gmail.com>
2023-07-25 08:22:49 -0300

    create  function to create gyms

--- src/repositories/inMemory/inMemoryGymsRepository.ts
+++ src/repositories/inMemory/inMemoryGymsRepository.ts
@@ -1,11 +1,27 @@
-import { Gym } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
 import { GymsRepository } from "../gyms-repository";
+import { randomUUID } from "node:crypto";
 
 export class InMemoryGymsRepository implements GymsRepository {
   public itens: Gym[] = []
 
   async findGymById(id: string): Promise<Gym | null> {
-    const user = this.itens.find(gym => gym.id === id)
-    return user ? user : null
+    const gym = this.itens.find(gym => gym.id === id)
+    return gym ? gym : null
+  }
+
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym: Gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+    }
+
+    this.itens.push(gym)
+
+    return gym
   }
 }
