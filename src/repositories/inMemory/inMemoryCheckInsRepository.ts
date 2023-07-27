f3e532da1f6094746de14838076b9df72e8d0cf0
allancassio <allan.cassio1@gmail.com>
2023-07-26 23:28:44 -0300

    create checkin repository to fetch history many checkins by user id

--- src/repositories/inMemory/inMemoryCheckInsRepository.ts
+++ src/repositories/inMemory/inMemoryCheckInsRepository.ts
@@ -1,8 +1,7 @@
-import { CheckIn, Prisma } from "@prisma/client";
-import { randomUUID } from "node:crypto";
-import { CheckInsRepository } from "../check-ins-repository";
-import { GetResult } from "@prisma/client/runtime";
-import dayjs from "dayjs";
+import { CheckIn, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { CheckInsRepository } from '../check-ins-repository'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckiInsRepository implements CheckInsRepository {
   public itens: CheckIn[] = []
@@ -13,31 +12,35 @@ export class InMemoryCheckiInsRepository implements CheckInsRepository {
       user_id: data.user_id,
       gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
-      created_at: new Date() 
+      created_at: new Date(),
     }
 
     this.itens.push(checkIn)
 
     return checkIn
   }
-  
+
   async findByUserIdOnDate(userId: string, date: Date) {
     // instalada lib dayjs para trabalhar com datas
     const startOfTheDay = dayjs(date).startOf('date') // startof retorna data YYYY-MM-DDT00:00:00
     const endOfTheDay = dayjs(date).endOf('date') // endOf retorna data YYYY-MM-DDT23:59:59
 
-    const checkInByUserId = this.itens.find(checkIn => {
+    const checkInByUserId = this.itens.find((checkIn) => {
       const checkInDate = dayjs(checkIn.created_at)
-      const isOnSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)  
-      
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
       return checkIn.user_id === userId && isOnSameDate
     })
 
-    if(!checkInByUserId) {
+    if (!checkInByUserId) {
       return null
     }
 
     return checkInByUserId
   }
 
+  async findManyByUserId(userId: string) {
+    return this.itens.filter((item) => item.user_id === userId)
+  }
 }
