3458268e6fbfec6235438b1553f6b746adb6770f
allancassio <allan.cassio1@gmail.com>
2023-08-10 11:06:19 -0300

    implement middleware verify jwt on profile route

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,10 @@
+import { makeGetUserProfileUseCase } from '@/useCases/factories/makeGetUserProfileUseCase'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase()
+  const { user } = await getUserProfile.execute({ userId: request.user.sub })
 
-  return reply.status(201).send()
+  return reply.status(201).send({ ...user, password_hash: undefined })
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import { authenticate } from '@/http/controllers/authenticate'
 import { profile } from '@/http/controllers/profile'
 import { register } from '@/http/controllers/register'
+import { verifyJWT } from '@/http/middlewares/verifyJWT'
 import { FastifyInstance } from 'fastify'
 
 export const userRoutes = async (app: FastifyInstance) => {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  app.get('/profile', profile)
+  app.get('/profile', { onRequest: [verifyJWT] }, profile)
 }
