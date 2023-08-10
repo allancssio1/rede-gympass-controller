3d50e6828fb1fbb21410250a7045f85eb7cceeef
allancassio <allan.cassio1@gmail.com>
2023-08-09 22:19:36 -0300

    create route profile

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,30 +1,30 @@
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-use-repository";
-import { AuthenticateUseCase } from "@/useCases/authenticate";
-import { InvalidCredentialsError } from "@/useCases/errors/invalidCredentialsError";
-import { makeAuthenticateUseCase } from "@/useCases/factoies/makeAuthenticateUseCase";
-import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
+import { InvalidCredentialsError } from '@/useCases/errors/invalidCredentialsError'
+import { makeAuthenticateUseCase } from '@/useCases/factories/makeAuthenticateUseCase'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 
-export async function authenticate (request: FastifyRequest, reply: FastifyReply) {
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
   const authenticateSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(6)
+    password: z.string().min(6),
   })
 
-  const {email,password} = authenticateSchema.parse(request.body)
+  const { email, password } = authenticateSchema.parse(request.body)
 
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({email, password})
+    await authenticateUseCase.execute({ email, password })
   } catch (error) {
-    if(error instanceof InvalidCredentialsError) {
-      return reply.status(400).send({message: error.message})
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
     }
 
     throw error
   }
 
   return reply.status(200).send()
-
 }
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,8 +1,11 @@
 import { authenticate } from '@/http/controllers/authenticate'
+import { profile } from '@/http/controllers/profile'
 import { register } from '@/http/controllers/register'
 import { FastifyInstance } from 'fastify'
 
 export const usersRoutes = async (app: FastifyInstance) => {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.get('/profile', profile)
 }
