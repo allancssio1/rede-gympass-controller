1986f048343dbaabb6f94019f7048eb12e93c246
allancassio <allan.cassio1@gmail.com>
2023-08-18 18:02:37 -0300

    create tests authenticate and profile

--- src/http/controllers/profile.spec.ts
+++ src/http/controllers/profile.spec.ts
@@ -1,3 +1,39 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Profile e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndow@exemple.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndow@exemple.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/profile')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(201)
+    expect(profileResponse.body).toEqual(
+      expect.objectContaining({
+        email: 'johndow@exemple.com',
+      }),
+    )
+  })
+})
